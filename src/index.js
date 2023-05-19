67a34632b415876fbbf1ac9627bb5b1aa7e6efe0
ZHumay <zehmetihumay@gmail.com>
2023-05-19 14:58:39 +0400

    basket

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './CartContext';
-
+import { ReactQueryDevtools } from 'react-query/devtools'
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
